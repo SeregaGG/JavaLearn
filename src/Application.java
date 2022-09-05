89f5da6eed1309d8093b6de8be86a9103ee18536
SeregaGG <48214766+SeregaGG@users.noreply.github.com>
2022-09-05 13:46:59 +0500

    kotik module add

--- src/Application.java
+++
@@ -1,8 +0,0 @@
-import model.Kotik;
-
-public class Application {
-    public static void main(String[] args){
-        Kotik test_kotik = new Kotik(10, 10, "Cat", "Nya");
-        System.out.println(test_kotik.prettiness);
-    }
-}
